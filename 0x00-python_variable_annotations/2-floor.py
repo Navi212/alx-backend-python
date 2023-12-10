77f02e344e735677bfc6eab82b883d061f09dd03
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 17:26:27 +0100

    Adds task 2 file

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import math
+""" The `2-floor` module supplies a function `floor`"""
+
+
+def floor(n: float) -> int:
+    """
+    floor: Function that returns the floor of a float
+
+    Args:
+    n: float
+
+    Return:
+    floor: int
+    """
+    return (math.floor(n))
