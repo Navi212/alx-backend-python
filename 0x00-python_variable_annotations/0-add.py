ac600a39971b364a0e7779b70ea4f3728036503a
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 17:03:04 +0100

    Adds README and task0 file

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+""" The `0-add` module supplies a function `add` """
+
+
+def add(a: float, b: float) -> float:
+    """
+    add: Function that retuns sum of `a` and `b`
+
+    Args:
+    a: float
+    b: float
+
+    Return:
+    sum: float
+    """
+    return (a + b)
---
+++ README.md
@@ -0,0 +1,1 @@
+# This repo contains all code base for ALX Python Backend (Specialization).
