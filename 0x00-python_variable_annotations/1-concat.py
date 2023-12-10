bcb99665294bf9ce985a6c60de6b264d4d7d0a03
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 17:18:13 +0100

    Adds task1 file

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" The `1-concat` module supplies a function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: Function that returns a concatenated string
+            `str1` and `str2`
+    Args:
+    str1: str
+    str2: str
+
+    Return:
+    concatenated string: str
+    """
+    return (str1 + str2)
