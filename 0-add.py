853c29097f0b56561eb6b1510f2f72702b52b188
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 16:41:37 +0100

    Adds task0 file

---
+++ 0-add.py
@@ -0,0 +1,15 @@
+""" The `0-add` module supplies a function add """
+
+
+def add(a: float, b: float) -> float:
+    """
+    add: Function returns the sum of `a` and `b`
+
+    Args:
+    a: float
+    b: float
+
+    Return:
+    sum: float
+    """
+    return (a + b)
