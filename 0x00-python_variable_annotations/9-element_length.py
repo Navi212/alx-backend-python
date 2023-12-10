4a89cb0ca413ed2e9313824ea46a0a35a8f59b53
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 21:58:49 +0100

    Adds task9 file

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" The `9-element_length` module supplies a function `element_length` """
+from typing import Iterable, List, Tuple, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    element_length: Function that takes iterable asa an argument
+                    Example of an iterable, a list
+
+    Args:
+    lst:            Iterable of Sequence
+
+    Return:
+    List            A list of tuples. Tuples of [sequence and ints]
+    """
+    return [(i, len(i)) for i in lst]
