c7ea816ff4cd2159f6ec25901879d479b63422d0
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 18:00:50 +0100

    modifies th e import line

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-from typing import List
 """ The `5-sum_list` module supplies a funcion `sum_list` """
+from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
