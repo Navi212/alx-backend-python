1036e44e6f2c971b1181dc0af8478ebab50d03db
Joseph Nweke <josephchristo615@gmail.com>
2023-12-10 17:38:18 +0100

    Adds task 3 file

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" The `3-to_str` module supplies a function `to_str` """
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: Function that returns the string representation
+            of argument `n`
+
+    Args:
+    n: float
+
+    Return:
+    string representation
+    """
+    return (str(n))
