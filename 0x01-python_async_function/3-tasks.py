eb15070fa0ebd4af732154ea0c84771b8e61a599
Joseph Nweke <josephchristo615@gmail.com>
2023-12-12 00:23:59 +0100

    Adds task 3 file

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" The `3-tasks` module supplies a function `task_wait_random` """
+import asyncio
+import time
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    task_wait_random: Function taking 1 arg `max_delay`
+                      and returns a newly created task
+
+    Args:
+    max_delay:        int
+
+    Return:
+    new Task:         asyncio.Task
+    """
+    new_task = asyncio.create_task(wait_random(max_delay))
+    return (new_task)
