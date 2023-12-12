994591cb0307ba38c0fac45bda9f7da1dd9b17fe
Joseph Nweke <josephchristo615@gmail.com>
2023-12-12 22:44:44 +0100

    Adds task 0 file

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ The `0-async_generator` module supplies a function `async_generator` """
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 
-async def async_generator() -> List[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async_generator: Function that takes no arg and returns
                      list of float
@@ -14,7 +14,7 @@ async def async_generator() -> List[float]:
     None
 
     Return:
-    List of floats
+    Generator
     """
     for _ in range(10):
         await asyncio.sleep(1)
