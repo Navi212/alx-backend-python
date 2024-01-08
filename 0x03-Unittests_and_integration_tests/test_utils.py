871c57244cf1752d850e8d0e6bc704032ebd18dd
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:31:45 +0100

    Adds method for task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,9 +10,18 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected_value):
         """ A test method """
         result = access_nested_map(nested_map, path)
         self.assertEqual(result, expected_value)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_val):
+        """ A test method implementing test with assertRaises """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
