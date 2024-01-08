719abb5680b7296bf77c3a4be6821350d884d1b9
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:07:51 +0100

    Updates tuple

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,15 +2,15 @@
 """ The `test_utils` supplies a class `TestAccessNestedMap` """
 import unittest
 from parameterized import parameterized
-from utils import *
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """  TestAccessNestedMap a test class """
     @parameterized.expand([
-        (({"a": 1}), ("a",), (1)),
-        (({"a": {"b": 2}}), ("a",), ({"b": 2})),
-        (({"a": {"b": 2}}), ("a", "b"), (2))
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_value):
         """ A test method """
