56c75d0a0aaaa016f3a4efa22406c22afd562e77
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:58:38 +0100

    Adds a class and method for task 2

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 """ The `test_utils` supplies a class `TestAccessNestedMap` """
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -25,3 +26,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """ A test method implementing test with assertRaises """
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """ A test class """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    def test_get_json(self, test_url, test_payload):
+        """
+        A test method that mocks request-response object
+        and sends the return value for response
+        """
+        response_mock = Mock()
+        response_mock.json.return_value = test_payload
+        with patch("utils.requests.get", return_value=response_mock):
+            result = get_json(test_url)
+            self.assertEqual(result, test_payload)
