a5380eec09dc5aca5ed8dcb48691ec38467efc71
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:16:36 +0100

    Adds self to assert

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,21 +6,13 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """_summary_
-
-    Args:
-        unittest (_type_): _description_
-    """
-
-    @parameterized.expand(
-        [
-            ({"a": 1}, ("a",), 1),
-            ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2)
-        ]
-    )
-    def test_access_nested_map(self, nested_map, path, expected_output):
-        """_summary_
-        """
+    """  TestAccessNestedMap a test class """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_value):
+        """ A test method """
         result = access_nested_map(nested_map, path)
-        self.assertEqual(result, expected_output)
+        self.assertEqual(result, expected_value)
