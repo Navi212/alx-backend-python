2bf5facef61771a77e4f64d2bdc859243172c26f
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:01:36 +0100

    Adds all files

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" The `test_utils` supplies a class `TestAccessNestedMap` """
+import unittest
+from parameterized import parameterized
+from utils import *
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """  TestAccessNestedMap a test class """
+    @parameterized.expand([
+        (({"a": 1}), ("a",), (1)),
+        (({"a": {"b": 2}}), ("a",), ({"b": 2})),
+        (({"a": {"b": 2}}), ("a", "b"), (2))
+    ])
+    def test_access_nested_map(self, nested_map, path, expected_value):
+        """ A test method """
+        assertEqual(access_nested_map(nested_map, path), expected_value)
