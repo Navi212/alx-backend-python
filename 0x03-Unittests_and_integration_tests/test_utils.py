5ca72dccda16bf80f012720f4bed3e0808afc237
Joseph Nweke <josephchristo615@gmail.com>
2024-01-08 13:13:23 +0100

    Updates class with docstring

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,13 +6,21 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """  TestAccessNestedMap a test class """
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
-    ])
-    def test_access_nested_map(self, nested_map, path, expected_value):
-        """ A test method """
+    """_summary_
+
+    Args:
+        unittest (_type_): _description_
+    """
+
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+        ]
+    )
+    def test_access_nested_map(self, nested_map, path, expected_output):
+        """_summary_
+        """
         result = access_nested_map(nested_map, path)
-        assertEqual(result, expected_value)
+        self.assertEqual(result, expected_output)
